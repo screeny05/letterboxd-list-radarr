82e7b6a26a41fd9d7daf5a6ad368b2ad8fc87dec
Sebastian Langer <langer@bestit-online.de>
2019-05-26 18:44:05 +0200

    add cache-lib

---
+++ lib/cache/index.ts
@@ -0,0 +1,38 @@
+import redis from 'redis';
+
+const cache = redis.createClient({ url: process.env.REDIS_URL });
+
+export const has = (key: string): Promise<boolean> => new Promise((resolve, reject) => {
+    cache.exists(key, (err, data) => {
+        if(err){
+            return reject(err);
+        }
+        resolve(data === 1);
+    });
+});
+
+export const get = <T = any>(key: string): Promise<T> => new Promise((resolve, reject) => {
+    cache.get(key, (err, data) => {
+        if(err){
+            return reject(err);
+        }
+        resolve(JSON.parse(data));
+    });
+});
+
+export const set = (key: string, value: any, ttl?: number): Promise<void> => new Promise((resolve, reject) => {
+    const cb = (err: any, data: any) => {
+        if(err){
+            return reject(err);
+        }
+        resolve();
+    };
+
+    value = JSON.stringify(value);
+
+    if(ttl){
+        cache.set(key, value, 'EX', ttl, cb);
+    } else {
+        cache.set(key, value, cb);
+    }
+});
